39502eebf0968c623428ca59e2b2ab7f16f308b6
Danil <daniklucky38@gmail.com>
2020-09-10 11:05:17 +0400

    add:score,styles

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
+    car.style.left = '125px';
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     for (let i = 0; i < getQuantityElements(100); i++){
         const line = document.createElement('div');
         line.classList.add('line');
@@ -47,7 +50,7 @@ function startGame() {
         enemy.style.top = enemy.y + 'px';
         gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
     setting.x = car.offsetLeft;
@@ -57,6 +60,9 @@ function startGame() {
 
 function playGame() {
     if(setting.start) {
+        score.style.display = 'block';
+        setting.score += setting.speed;
+        score.textContent = 'SCORE: ' + setting.score;
         moveRoad();
         moveEnemy();
         if(keys.ArrowLeft && setting.x > 0){
@@ -105,7 +111,17 @@ function moveRoad() {
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left && carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top){
+            setting.start = false;
+            console.warn('ДТП ПИСЮНЬЧИК');
+            start.classList.remove('hide');
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
 
@@ -114,6 +130,4 @@ function moveEnemy() {
             item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
         }
     })
-
-    
 }
